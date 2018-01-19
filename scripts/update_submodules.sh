9d72e5e281854c6b6dfadcee64fd12b78a50d7a4
romain zanchi <romainzanchi30@gmail.com>
2018-01-19 17:42:50 +0100

    updated submodules script & added submodules

--- scripts/update_submodules.sh
+++ scripts/update_submodules.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "===> updating git submodules"
+git submodule add --force https://github.com/trackit/trackit2-client trackit2-client
+git submodule add --force https://github.com/trackit/trackit2 trackit2
 git submodule update --recursive --remote
